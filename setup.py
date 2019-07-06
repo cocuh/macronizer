9d38a6e546b18ff08dc3b9403b6aa06c802d3412
Kosuke Kusano <cocuh.kk@gmail.com>
2019-07-06 21:07:29 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='micronizer',
+  version='',
+  packages=find_packages(),
+  url='',
+  license='',
+  author='koku',
+  author_email='',
+  description=''
+)
